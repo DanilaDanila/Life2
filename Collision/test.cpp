3868833b731567c8b2520b4697e26faaf67ee986
Danila <manakov.danila@mail.ru>
2018-06-11 11:57:51 +0300

    Create window.

--- Collision/test.cpp
+++ Collision/test.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <OpenGl/OpenGL.h>
+#include <GLUT/GLUT.h>
 #include "line.hpp"
 
 using namespace mucl;
 
-int main(int argc, char ** argv)
+void display()
 {
-	line l0(vec2(0,1),vec2(1,0));
-	line l1(vec2(2,1),vec2(-1,0));
-	std::cout<<l0.isIntersects(l1)<<'\n';
-	vec2 v=l0.intersects(l1);
-	std::cout<<v.x<<' '<<v.y<<'\n';
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glFlush();
+}
+
+void init(int argc, char **argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(1024, 576);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("");
+
+	gluOrtho2D(0.0, 1024.0, 576.0, 0.0);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+}
+
+int main(int argc, char **argv)
+{
+	init(argc,argv);
+
+	glutMainLoop();
 	return 0;
 }
